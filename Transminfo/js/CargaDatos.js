cd1d409f2246b5310aefea04e46baa8affba64f0
jorge <jorgearojas25@gmail.com>
2019-04-02 14:33:04 -0500

    Busqueda de rutas

--- Transminfo/js/CargaDatos.js
+++ Transminfo/js/CargaDatos.js
@@ -5,11 +5,19 @@ var db = new loki('trasmilenio.db', {
   autosaveInterval: 4000
 });
     var estaciones = db.addCollection('estaciones');
+    var vagones = db.addCollection('vagones');
+    var rutas = db.addCollection('rutas');
 
     
     function databaseInitialize() {
       if (!db.getCollection('estaciones')) {
         db.addCollection('estaciones');
+      }
+      if (!db.getCollection('vagones')) {
+        db.addCollection('vagones');
+      }
+      if (!db.getCollection('rutas')) {
+        db.addCollection('rutas');
       }
     }
 
@@ -23,17 +31,98 @@ var db = new loki('trasmilenio.db', {
 
         data.result.records.forEach(item => {
           estaciones.insert({
-            Estacion: item.Name,
+            NameEstacion: item.Name,
             Troncal: item.Corredor,
-            Codigo: item.Id,
-            LatLon: { Lat: item.Latitud, Lon: item.Longitud },
-            Rutas:[]
+            idEstacion: item.Id,
+            LatLon: { Lat: item.Latitud, Lon: item.Longitud }
           });
         });
         
       });
 
 
+      vagones.insert([
+        {idVagon:'Olaya1', Name:'1', NameEstacion:'Olaya'},
+        {idVagon:'Olaya2', Name:'2', NameEstacion:'Olaya'},
+        {idVagon:'Narino1', Name:'1', NameEstacion:'Nariño'},
+        {idVagon:'Narino2', Name:'2', NameEstacion:'Nariño'},
+        {idVagon:'Campina1',Name:'1', NameEstacion:'Campina'},
+        {idVagon:'Campina2',Name:'2', NameEstacion:'Campina'},
+        {idVagon:'Campina3',Name:'3', NameEstacion:'Campina'},
+        {idVagon:'Campina4',Name:'4', NameEstacion:'Campina'},
+        {idVagon:'tv911',Name:'1', NameEstacion:'Tv 91'},
+        {idVagon:'tv912',Name:'1', NameEstacion:'Tv 91'},
+        {idVagon:'tv913',Name:'1', NameEstacion:'Tv 91'},
+        {idVagon:'tv914',Name:'1', NameEstacion:'Tv 91'}
+
+      ]);
+
+      rutas.insert([
+        {idRuta:'16_H15', Name:'H15', idVagon:'Olaya2'},
+        {idRuta:'15_H15', Name:'H15', idVagon:'Narino2'},
+        {idRuta:'16_H21', Name:'H21', idVagon:'Olaya1'},
+        {idRuta:'12_H73', Name:'H73', idVagon:'Olaya1'},
+        {idRuta:'19_C19', Name:'C19', idVagon:'Campina4'},
+        {idRuta:'2_B50', Name:'B50', idVagon:'Campina2'},
+        {idRuta:'12_C50', Name:'B50', idVagon:'Campina3'},
+        {idRuta:'2_C17', Name:'C17', idVagon:'Campina4'},
+        {idRuta:'20_H17', Name:'H17', idVagon:'Campina1'},
+        {idRuta:'2_F19', Name:'F19', idVagon:'Campina1'},
+        {idRuta:'18_C29', Name:'C29', idVagon:'Campina1'},
+        {idRuta:'2_7', Name:'7', idVagon:'Campina2'},
+        {idRuta:'2_F29', Name:'F29', idVagon:'Campina1'},
+        {idRuta:'2_G3O', Name:'G30', idVagon:'Campina3'},
+        {idRuta:'16_C3O', Name:'C30', idVagon:'Campina2'},
+        {idRuta:'2_J73', Name:'J73', idVagon:'Campina1'},
+        {idRuta:'8_C73', Name:'C73', idVagon:'Campina3'},
+        {idRuta:'3_7', Name:'7', idVagon:'tv912'},
+        {idRuta:'17_C29', Name:'C29', idVagon:'tv914'},
+        {idRuta:'3_F29', Name:'F29', idVagon:'tv911'},
+        {idRuta:'3_J73', Name:'J73', idVagon:'tv911'},
+        {idRuta:'7_C73', Name:'C73', idVagon:'tv913'},
+        {idRuta:'3_G30', Name:'G30', idVagon:'tv912'},
+        {idRuta:'15_C30', Name:'C30', idVagon:'tv913'},
+        {idRuta:'3_F19', Name:'F19', idVagon:'tv911'},
+        {idRuta:'18_C19', Name:'C19', idVagon:'tv914'},
+        {idRuta:'3_B50', Name:'B50', idVagon:'tv911'},
+        {idRuta:'11_C50', Name:'C50', idVagon:'tv914'}
+      ]);
+
+     
+
+      var testArray = [{id:"Olaya",troncal:"Caracas",test:"1"}];
+
+      function verVagones(dato_nombreEstacion){
+        let vagones = db.getCollection('vagones');
+
+        var vagonesFiltrados = $.grep(vagones.data,function(value){
+          return value.NameEstacion == dato_nombreEstacion;
+        });
+         $.each(vagonesFiltrados,function(index,value){
+          verRutas(value.idVagon);
+        });
+        
+        //console.log(vagonesFiltrados);
+      }
+
+
+      function verRutas(dato_idVagon){
+
+        let rutas = db.getCollection('rutas');
+
+        var rutasFiltradas = $.grep(rutas.data,function(value){
+          return value.idVagon == dato_idVagon;
+        });
+        console.log(rutasFiltradas);
+        //$("#rutaspop").empty();
+        var html = '';
+        $.each(rutasFiltradas,function(index,value){
+        html += `<div style="margin-top:10px"> <ul><li>' ${value.Name} '</li> <ul></div>`
+        });
+        $("#rutaspop").append(html);
+      }
+
+      //console.log(mostrarRutas('Olaya1'));
 
     function Ver(darta) {
         document.getElementById(darta).innerHTML = '';
@@ -46,7 +135,7 @@ var db = new loki('trasmilenio.db', {
         const card = document.createElement('ul');
         card.setAttribute('class', 'card');
         const li = document.createElement('li');
-        li.textContent = item.Estacion;
+        li.textContent = item.NameEstacion;
         container.appendChild(card);
         card.appendChild(li);
       });
@@ -63,7 +152,7 @@ var db = new loki('trasmilenio.db', {
         const card = document.createElement('ul');
         card.setAttribute('class', 'card');
         const li = document.createElement('li');
-        li.textContent = item.Estacion;
+        li.textContent = item.NameEstacion;
         container.appendChild(card);
         card.appendChild(li);
       });
